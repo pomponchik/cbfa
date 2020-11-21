b25ee8610908b3764522bcfdaaf6f7823046ab36
Annara Halmon <ahalmon-@il-c2.msk.21-school.ru>
2020-11-21 23:44:10 +0300

    readme

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="cbfa",
+    version="0.0.1",
+    author="Evgeniy Blinov",
+    author_email="zheni-b@yandex.ru",
+    description="Class-based views for the FastAPI",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pomponchik/cbfa",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
