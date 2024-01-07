1e650e30dbe565d663388f40987e217d10d90039
Evgeniy Blinov <zheni-b@yandex.ru>
2024-01-07 21:39:39 +0300

    type hints

--- cbfa/class_based.py
+++ cbfa/class_based.py
@@ -23,15 +23,15 @@ class ClassBased:
         def decorator(Class: Type[Any]) -> Type[Any]:
             for method_name in self.methods:
                 if hasattr(Class, method_name):
-                    method = self.get_method(method_name)
-                    if method is not None:
-                        method = method(*args, **kwargs)
+                    fastapi_decorator_fabrique = self.get_fastapi_decorator_fabrique(method_name)
+                    if fastapi_decorator_fabrique is not None:
+                        fast_api_decorator = fastapi_decorator_fabrique(*args, **kwargs)
                         new_method = getattr(Class, method_name)
                         new_method.__name__ = f'{Class.__name__.lower()}_{method_name}'
-                        new_method = method(new_method)
+                        new_method = fast_api_decorator(new_method)
                         setattr(Class, method_name, new_method)
             return Class
         return decorator
 
-    def get_method(self, method_name: str) -> Optional[Callable[..., Any]]:
+    def get_fastapi_decorator_fabrique(self, method_name: str) -> Optional[Callable[..., Any]]:
         return getattr(self.app, method_name, None)
