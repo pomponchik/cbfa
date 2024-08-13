437b5d153793ed0abe7a6777c843db4de18c0b80
Juris Krumgolds <juris.krumgolds@gmail.com>
2024-08-13 09:46:43 +0300

    Support for summary and docstring defaults in OpenAPI

--- cbfa/class_based.py
+++ cbfa/class_based.py
@@ -1,4 +1,5 @@
 import functools
+import re
 from typing import Callable, Type, Optional, Any
 
 from fastapi import FastAPI
@@ -27,10 +28,14 @@ class ClassBased:
                 if hasattr(instance, method_name):
                     fastapi_decorator_fabrique = self.get_fastapi_decorator_fabrique(method_name)
                     if fastapi_decorator_fabrique is not None:
+                        if 'summary' not in kwargs:
+                            kwargs['summary'] = ' '.join(re.findall('[A-Z][^A-Z]*', Class.__name__))
                         fast_api_decorator = fastapi_decorator_fabrique(*args, **kwargs)
                         new_method = getattr(instance, method_name)
+                        docstring = new_method.__doc__
                         new_method = functools.partial(new_method.__func__, instance)
                         new_method.__name__ = f'{Class.__name__.lower()}_{method_name}'
+                        new_method.__doc__ = docstring
                         new_method = fast_api_decorator(new_method)
                         setattr(instance, method_name, new_method)
             return Class
